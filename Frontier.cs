452b545c33c863b17ae40e5d62d18fc2132ba589
JamieKoz <81804442+JamieKoz@users.noreply.github.com>
2021-06-23 16:38:22 +1000

    Add files via upload

---
+++ Frontier.cs
@@ -0,0 +1,100 @@
+
+using System.Collections.Generic;
+using System.Linq;
+
+namespace NPuzzle
+{
+    public class Frontier
+    {
+        //We can use a linked list here because it's more efficient than an array
+        //and because we only want to add/remove items from the start or end.
+        private LinkedList<PuzzleState> Items = new LinkedList<PuzzleState>();
+
+        public int Count => Items.Count;
+
+        /// <summary>
+        /// Removes items from the top of the frontier.
+        /// </summary>
+        /// <returns></returns>
+        public PuzzleState Pop()
+        {
+            var item = Items.First();
+            Items.RemoveFirst();
+            return item;
+        }
+
+        /// <summary>
+        /// Add a item to the top of the frontier
+        /// </summary>
+        /// <param name="state"></param>
+        public void Push(PuzzleState state)
+        {
+            Items.AddFirst(state);
+        }
+
+        public void Push(PuzzleState[] states)
+        {
+            foreach (PuzzleState state in states)
+            {
+                Push(state);
+            }
+        }
+
+        /// <summary>
+        ///  Add a item to the bottom the frontier
+        /// </summary>
+        /// <param name="aState"></param>
+        public void Enqueue(PuzzleState state)
+        {
+            Items.AddLast(state);
+        }
+
+        /// <summary>
+        /// Add multiple items to the bottom of the frontier.
+        /// Index zero will be enqueued first followed by the rest
+        /// of the elements in the array
+        /// </summary>
+        /// <param name="states"></param>
+        public void Enqueue(PuzzleState[] states)
+        {
+            foreach (PuzzleState state in states)
+            {
+                Enqueue(state);
+            }
+        }
+
+        public void Clear()
+        {
+            Items.Clear();
+        }
+
+        public bool Contains(PuzzleState state)
+        {
+            return Items.Contains(state);
+        }
+
+        public void SortByHeuristicAsc()
+        {
+            var tempList = new LinkedList<PuzzleState>();
+            var sortedItems = Items.OrderBy(i => i.EvaluationFunction);
+
+            foreach (var item in sortedItems)
+            {
+                tempList.AddLast(item);
+            }
+
+            Items = tempList;
+        }
+
+        public void SortByHeuristicDesc()
+        {
+            var tempList = new LinkedList<PuzzleState>();
+            var sortedItems = Items.OrderByDescending(i => i.EvaluationFunction);
+            foreach (var item in sortedItems)
+            {
+                tempList.AddLast(item);
+            }
+            Items = tempList;
+        }
+    }
+}
---
+++ PuzzleState.cs
@@ -0,0 +1,351 @@
+using System;
+using System.Collections.Generic;
+using NPuzzle.Exceptions;
+
+namespace NPuzzle
+{
+    public class PuzzleState : IEquatable<PuzzleState>
+    {
+        public int[,] Puzzle;
+        public PuzzleState Parent;
+        public List<PuzzleState> Children;
+        public int Cost;
+        public int HeuristicValue;
+        public int EvaluationFunction { get; set; }
+        public Direction PathFromParent;
+
+        public PuzzleState(PuzzleState parent, Direction fromParent, int[,] puzzle)
+        {
+            Parent = parent;
+            PathFromParent = fromParent;
+            Puzzle = puzzle;
+            Cost = Parent.Cost + 1;
+            EvaluationFunction = 0;
+            HeuristicValue = 0;
+        }
+
+        public PuzzleState(int[,] puzzle)
+        {
+            Parent = null;
+            PathFromParent = Direction.None;
+            Cost = 0;
+            Puzzle = puzzle;
+            EvaluationFunction = 0;
+            HeuristicValue = 0;
+        }
+
+        public List<Direction> GetPossibleActionsAdjusted()
+        {
+            int[] blankLocation = { 0, 0 }; //dummy value to avoid errors.
+
+            try
+            {
+                //find where the blank cell is and store the directions.
+                blankLocation = FindBlankCell();
+            }
+            catch (InvalidPuzzleException)
+            {
+                Console.WriteLine("There was an error in processing! Aborting...");
+                Environment.Exit(1);
+            }
+
+            var possibleMoves = new List<Direction>();
+
+            // M check
+            // M is horizontal. N is veritcal.
+            int blankTileNPos = blankLocation[0];
+            int blankTileMPos = blankLocation[1];
+
+
+            if (blankTileMPos != 0) // Not at the top.
+            {
+                possibleMoves.Add(Direction.Up);
+            }
+            if (blankTileNPos != 0) // Not on the far left boundry.
+            {
+                possibleMoves.Add(Direction.Left);
+            }
+            if (blankTileMPos != (Puzzle.GetLength(1) - 1)) // Not at the bottom.
+            {
+                possibleMoves.Add(Direction.Down);
+            }
+            if (blankTileNPos != (Puzzle.GetLength(0) - 1)) // Not at the far right boundry.
+            {
+                possibleMoves.Add(Direction.Right);
+            }
+
+            return possibleMoves;
+        }
+
+
+        public Direction[] GetPossibleActions()
+        {
+            int[] blankLocation = { 0, 0 }; //dummy value to avoid errors.
+
+            try
+            {
+                //find where the blank cell is and store the directions.
+                blankLocation = FindBlankCell();
+            }
+            catch (InvalidPuzzleException)
+            {
+                Console.WriteLine("There was an error in processing! Aborting...");
+                Environment.Exit(1);
+            }
+
+            Direction[] result = new Direction[CountMovements(blankLocation)];
+
+            int thisIndex = 0;
+
+            // M check
+            // M is horizontal. N is veritcal.
+            int blankTileNPos = blankLocation[0];
+            int blankTileMPos = blankLocation[1];
+
+
+            if (blankTileNPos == 0)
+            {
+                //the blank cell is already as far left as it will go, it can move right
+                result[thisIndex++] = Direction.Right;
+            }
+            else if (blankTileNPos == (Puzzle.GetLength(0) - 1))
+            {
+                result[thisIndex++] = Direction.Left;
+            }
+            else
+            {
+                result[thisIndex++] = Direction.Left;
+                result[thisIndex++] = Direction.Right;
+            }
+
+            // Y Check
+            if (blankTileMPos == 0)
+            {
+                //the blank cell is already as far up as it will go, it can move down
+                result[thisIndex++] = Direction.Down;
+            }
+            else if (blankTileMPos == (Puzzle.GetLength(1) - 1))
+            {
+                result[thisIndex++] = Direction.Up;
+            }
+            else
+            {
+                result[thisIndex++] = Direction.Up;
+                result[thisIndex++] = Direction.Down;
+            }
+            return result;
+        }
+
+        private int[] FindBlankCell()
+        {
+            for (int i = 0; i < Puzzle.GetLength(0); i++)
+            {
+                for (int j = 0; j < Puzzle.GetLength(1); j++)
+                {
+                    if (Puzzle[i, j] == 0)
+                    {
+                        int[] result = { i, j };
+                        return result;
+                    }
+                }
+            }
+            //No blank cell found?
+            throw new InvalidPuzzleException();
+        }
+
+        private int CountMovements(int[] blankLocation)
+        {
+            int result = 2;
+            try
+            {
+                blankLocation = FindBlankCell();
+
+                for (int i = 0; i <= 1; i++)
+                {
+                    if (blankLocation[i] == 0 || blankLocation[i] == (Puzzle.GetLength(i) - 1))
+                    {
+                        //do nothing
+                    }
+                    else
+                    {
+                        result++;
+                    }
+                }
+            }
+            catch (InvalidPuzzleException)
+            {
+                //do something
+            }
+            return result;
+        }
+
+        private int[,] CloneArray(int[,] cloneMe)
+        {
+            int[,] result = new int[cloneMe.GetLength(0), cloneMe.GetLength(1)];
+
+            for (int i = 0; i < cloneMe.GetLength(0); i++)
+            {
+                for (int j = 0; j < cloneMe.GetLength(1); j++)
+                {
+                    result[i, j] = cloneMe[i, j];
+                }
+            }
+            return result;
+        }
+
+        public PuzzleState Move(Direction direction)
+        {
+            //Moving up moves the empty cell up (and the cell above it down)
+            //first, create the new one (the one to return)
+            PuzzleState result = new PuzzleState(this, direction, CloneArray(this.Puzzle));
+
+            //now, execute the changes: move the blank cell aDirection
+            //find the blankCell
+            int[] blankCell = { 0, 0 };
+            try
+            {
+                blankCell = FindBlankCell();
+            }
+            catch (InvalidPuzzleException)
+            {
+                Console.WriteLine("There was an error in processing! Aborting...");
+                Environment.Exit(1);
+            }
+            try
+            {
+                //move the blank cell in the new child puzzle
+
+                if (direction == Direction.Up)
+                {
+                    result.Puzzle[blankCell[0], blankCell[1]] = result.Puzzle[blankCell[0], blankCell[1] - 1];
+                    result.Puzzle[blankCell[0], blankCell[1] - 1] = 0;
+                }
+                else if (direction == Direction.Down)
+                {
+                    result.Puzzle[blankCell[0], blankCell[1]] = result.Puzzle[blankCell[0], blankCell[1] + 1];
+                    result.Puzzle[blankCell[0], blankCell[1] + 1] = 0;
+                }
+                else if (direction == Direction.Left)
+                {
+                    result.Puzzle[blankCell[0], blankCell[1]] = result.Puzzle[blankCell[0] - 1, blankCell[1]];
+                    result.Puzzle[blankCell[0] - 1, blankCell[1]] = 0;
+                }
+                else    //aDirection == Right;
+                {
+                    result.Puzzle[blankCell[0], blankCell[1]] = result.Puzzle[blankCell[0] + 1, blankCell[1]];
+                    result.Puzzle[blankCell[0] + 1, blankCell[1]] = 0;
+                }
+
+                return result;
+            }
+            catch (IndexOutOfRangeException)
+            {
+                throw new CantMoveThatWayException();
+            }
+        }
+
+        public List<PuzzleState> Explore()
+        {
+            //populate children
+            Direction[] possibleMoves = GetPossibleActionsAdjusted().ToArray();
+            Children = new List<PuzzleState>();
+            for (int i = 0; i < possibleMoves.Length; i++)
+            {
+                try
+                {
+                    Children.Add(Move(possibleMoves[i]));
+                }
+                catch (CantMoveThatWayException)
+                {
+                    Console.WriteLine("There was an error in processing! Aborting...");
+                    Environment.Exit(1);
+                }
+            }
+            return Children;
+        }
+        public List<PuzzleState> ExploreDFS()
+        {
+            //populate children
+            Direction[] possibleMoves = GetPossibleActions();
+            Children = new List<PuzzleState>();
+            for (int i = 0; i < possibleMoves.Length; i++)
+            {
+                try
+                {
+                    Children.Add(Move(possibleMoves[i]));
+                }
+                catch (CantMoveThatWayException)
+                {
+                    Console.WriteLine("There was an error in processing! Aborting...");
+                    Environment.Exit(1);
+                }
+            }
+            return Children;
+        }
+
+        public Direction[] GetPathToState()
+        {
+            Direction[] result;
+
+            if (Parent == null) //If this is the root node, there is no path!
+            {
+                result = new Direction[0];
+                return result;
+            }
+            else                //Other wise, path to here is the path to parent
+                                // plus parent to here
+            {
+                Direction[] pathToParent = Parent.GetPathToState();
+                result = new Direction[pathToParent.Length + 1];
+                for (int i = 0; i < pathToParent.Length; i++)
+                {
+                    result[i] = pathToParent[i];
+                }
+                result[result.Length - 1] = this.PathFromParent;
+                return result;
+            }
+        }
+
+        public bool Equals(PuzzleState other)
+        {
+            //evaluate if these states are the same (does this.Puzzle == aState.Puzzle)?
+            for (int i = 0; i < Puzzle.GetLength(0); i++)
+            {
+                for (int j = 0; j < Puzzle.GetLength(1); j++)
+                {
+                    if (this.Puzzle[i, j] != other.Puzzle[i, j])
+                        return false;       //stop checking as soon as we find an
+                                            // element that doesn't match
+                }
+            }
+            return true;	//All elements matched? Return true
+        }
+
+        public override string ToString()
+        {
+            int rowsLength = Puzzle.GetLength(1);
+            int colsLenth = Puzzle.GetLength(0);
+
+            string result = "     ";
+            for (int i = 0; i < colsLenth; i++)
+            {
+                result += $"{i}  ";
+            }
+            result += Environment.NewLine;
+
+            for (int i = 0; i < rowsLength; i++)
+            {
+                result += $"{i}: [ ";
+
+                for (int j = 0; j < colsLenth; j++)
+                {
+                    result += $"{Puzzle[j, i]}";
+                    if (j != colsLenth - 1)
+                        result += ", ";
+                }
+                result += " ]" + Environment.NewLine;
+            }
+            return result;
+        }
+    }
+}
